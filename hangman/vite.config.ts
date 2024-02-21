b0f8746243da187aae0ac0c042a5427d682ab447
JustBuster <sauravsingh527@gmail.com>
2024-02-20 23:31:20 -0330

    Fixed building error

--- hangman/vite.config.ts
+++ hangman/vite.config.ts
@@ -4,6 +4,9 @@ import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+      outDir: 'dist',
+  },
   plugins: [react(
       {
           include: "**/*.tsx",
